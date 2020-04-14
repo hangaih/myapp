12d0abece1c791fb2400e71ee56add2b7c8a6357
hangaih <hangai.tech@gmail.com>
2020-04-14 21:28:26 +0900

    dockerの練習

---
+++ ruby_practice/ruby_book/lib/block_practice.rb
@@ -0,0 +1,3 @@
+number = [1, 2, 3, 4, 5]
+new_numbers = number.map{ |n| n * 10 }
+p new_numbers
---
+++ ruby_practice/sample2.rb
@@ -0,0 +1,11 @@
+# Userクラスを定義する
+class User
+  attr_reader :first_name, :last_name, :age
+
+  def initialize{first_name, last_name, :age}
+    @first_name = first_name
+    @last_name = last_name
+    @age = age
+  end
+end
+
